186a874ed10750e1d725b5fad9cc0ef295db15ac
rgalindo33 <rgalindo33@hotmail.com>
2013-10-11 12:06:04 +0200

    first commit

---
+++ lib/default.rb
@@ -0,0 +1,11 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+
+def tags
+  if @item[:tags].nil?
+    '(none)'
+  else
+    @item[:tags].join(', ')
+  end
+end
